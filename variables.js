23873462274b03e75222352a45723396be0114e9
alvaro aguirre <alguirre2@gmail.com>
2023-09-11 11:31:08 -0600

    03 variables

---
+++ variables.js
@@ -0,0 +1,27 @@
+//versiones anterios a ES6 era var
+var miVariable;
+
+// de ES6 en adelante 
+// const y let
+
+//conts declara constantes, no permite reaccinacion
+// al crearla debe de iniciarlizarse
+const PI=3.1415;
+//PI =3.28;
+
+// let decalra variables
+// permite reaccionacion
+// se puede declarar y luego inicializar 
+// JS detecta automaticamente el tipo de dato
+let firstname;
+
+console.log(firstname);
+
+firstname="peter Parker"
+console.log(firstname);
+console.log(typeof(firstname));
+
+firstname= 18;
+console.log(firstname);
+console.log(typeof(firstname));
+
