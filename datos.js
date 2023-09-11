765dc3c13af8c48b95562c74c8bcffd1b63771af
alvaro aguirre <alguirre2@gmail.com>
2023-09-11 11:20:58 -0600

    02 tipos de Datos en JS

---
+++ datos.js
@@ -0,0 +1,19 @@
+console.log("ejecutando desde Node jajajaj");
+
+// tipos de datos en js
+console.log(12);
+console.log(typeof(12));
+console.log(12.34);
+console.log(typeof(12.34));
+console.log(true);
+console.log(typeof(true));
+console.log("este es un String");
+console.log(typeof("este es un String"));
+let variable;
+console.log(variable);
+console.log(typeof(variable));
+console.log([]);
+console.log(typeof([]));
+
+console.log([1, 2, 3])
+console.log({name: "jose"})
